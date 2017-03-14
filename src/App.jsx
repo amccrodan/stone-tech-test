491de84653f7bd068294a1c9e6b4ff8bffeb0d93
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-03-13 17:23:42 -0700

    Continued scaffold of project with webpack-dev-server.

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+  }
+
+}
+export default App;
