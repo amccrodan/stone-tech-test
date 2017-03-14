d3ad7405c27b1795ed75b80af0e832c5fab72902
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-03-13 17:39:59 -0700

    Updated webpack output folder and added getMessages function to App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
 import React, {Component} from 'react';
+import MessageList from "./MessageList.jsx";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      messages: []
     };
+  }
+
+  getMessages() {
+    fetch('https://andrew-mccrodan-test.herokuapp.com/messages')
+    .then((response) => {
+      return response.json();
+    }).then((data) => {
+      this.setState({messages: data.results}, () => {
+        console.log(this.state.messages);
+      })
+    }).catch(() => {
+      console.log('Error in getMessages!');
+    })
   }
 
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <MessageList />
       </div>
     );
   }
 
   componentDidMount() {
+    this.getMessages();
     console.log("componentDidMount <App />");
   }
 
